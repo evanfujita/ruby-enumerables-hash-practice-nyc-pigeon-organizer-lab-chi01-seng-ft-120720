8a3916955ce67f60c5b14dfc06f340bef9a77f5b
Evan Fujita <evanfujita@gmail.com>
2020-11-10 04:00:12 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 def nyc_pigeon_organizer(data)
   hash = data.each_with_object({}) do |(key, value), hash| 
-    value.each do |key1, value1| key.to_s
+    value.each do |key1, value1| key1.to_s
       value1.each do |name| 
         if !hash[name]
           hash[name] = {}
