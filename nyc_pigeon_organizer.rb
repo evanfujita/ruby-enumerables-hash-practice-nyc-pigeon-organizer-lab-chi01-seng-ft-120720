face63c76c439a9841d95cdece98f3a90616209f
Evan Fujita <evanfujita@gmail.com>
2020-11-10 03:59:36 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -10,7 +10,7 @@ def nyc_pigeon_organizer(data)
       if !hash[name][key]
         hash[name][key] = []
       end
-      hash[name][key].push(key1)
+      hash[name][key].push(key1).flatten
   end
 end
 end
